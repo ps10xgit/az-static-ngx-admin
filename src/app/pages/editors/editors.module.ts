478ecaf825ed84a7e4ac2732e3d15f2706e50ff8
ps10xgit <pshinnick03@gmail.com>
2025-04-09 08:42:21 +0200

    update ckeditor

--- src/app/pages/editors/editors.module.ts
+++ src/app/pages/editors/editors.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NbCardModule } from '@nebular/theme';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CKEditorModule } from 'ckeditor4-angular';
 
 import { ThemeModule } from '../../@theme/theme.module';
 
