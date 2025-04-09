9c8a745174b766c2e3f8b9a2e9cb7aae37bc396b
ps10xgit <pshinnick03@gmail.com>
2025-04-09 08:42:04 +0200

    update tinymce

--- src/app/@theme/components/tiny-mce/tiny-mce.component.ts
+++ src/app/@theme/components/tiny-mce/tiny-mce.component.ts
@@ -20,8 +20,7 @@ export class TinyMCEComponent implements OnDestroy, AfterViewInit {
   ngAfterViewInit() {
     tinymce.init({
       target: this.host.nativeElement,
-      plugins: ['link', 'paste', 'table'],
-      skin_url: `${this.locationStrategy.getBaseHref()}assets/skins/lightgray`,
+      plugins: ['link', 'table'],
       setup: editor => {
         this.editor = editor;
         editor.on('keyup', () => {
